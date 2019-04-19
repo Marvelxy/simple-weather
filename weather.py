eee795781aaeeaedbb72c12042a8ce11e799ae09
Marvelous Akpotu <still4marvelous@gmail.com>
2019-04-19 06:47:27 +0100

    Initial commit

---
+++ weather.py
@@ -0,0 +1,59 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+def simple_get(url):
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+    
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+def is_good_response(resp):
+    content_type = resp.headers['Content-Type'].lower()
+    return(resp.status_code == 200 and content_type is not None and content_type.find('html') > -1)
+
+def log_error(e):
+    print(e)
+    
+    
+raw_html = simple_get('https://forecast.weather.gov/MapClick.php?lat=42.3587&lon=-71.0567')
+
+html = BeautifulSoup(raw_html, 'html.parser')
+target = html.find(id='detailed-forecast-body')
+
+
+text_forcast_label = []
+forcast_label = target.find_all('div', class_='forecast-label');
+# Create a list of forcast label
+for l in forcast_label:
+    #print(l.get_text())
+    text_forcast_label.append(l.get_text())
+    
+text_forcast_text = []
+forcast_text = target.find_all('div', class_='forecast-text');
+# Create a list of forcast text
+for t in forcast_text:
+    #print(t.get_text())
+    text_forcast_text.append(t.get_text())
+
+#print(text_forcast_label)
+#print(text_forcast_text)
+
+detailed_forcast = {}
+
+for fl in text_forcast_label:
+    for ft in text_forcast_text:
+        if text_forcast_label.index(fl) == text_forcast_text.index(ft):
+            detailed_forcast[fl] = ft
+
+#print(detailed_forcast)
+
+for day in detailed_forcast:
+    print(day + ' : ' + detailed_forcast[day], end='\n')
